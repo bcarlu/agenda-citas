9dee31962e6f90ad26cb14033796e724ef71733b
Brian <briancardona87@gmail.com>
2025-11-25 10:40:00 -0500

    refactor: Se actualiza ruta de url para que funcione al agregar el nav en cualquier archivo independiente de su ubicacion.

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <!--Se valida rol del usuario-->
-    <?php $inicio = $_SESSION['id_rol'] === 1 ? "inicio_admin.php" : "inicio.php"; ?>
-    <a class="navbar-brand" href=<?php echo $inicio; ?>><img src="img/user.png" width="30" height="30" class="d-inline-block align-top" alt=""><small class="pl-2"><?php echo htmlspecialchars($_SESSION['nombre_usuario'], ENT_QUOTES); ?></small></a>
+    <?php $inicio = $_SESSION['id_rol'] === 1 ? "/inicio_admin.php" : "/inicio.php"; ?>
+    <a class="navbar-brand" href=<?php echo $inicio; ?>><img src="/img/user.png" width="30" height="30" class="d-inline-block align-top" alt=""><small class="pl-2"><?php echo htmlspecialchars($_SESSION['nombre_usuario'], ENT_QUOTES); ?></small></a>
     
     <span>
     <?php setlocale(LC_TIME,'es_CO.utf8'); 
@@ -14,7 +14,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link float-right text-danger" href="php/cerrarSesion.php">Cerrar sesion </a>
+                <a class="nav-link float-right text-danger" href="/php/cerrarSesion.php">Cerrar sesion </a>
             </li>
                 
         </ul>
