6bbaa4fdb75d4255d36f2b7d260336f075e19d2f
Brian <briancardona87@gmail.com>
2025-12-05 16:59:35 -0500

    feat: Se agrega funcion para resaltar el item del menu lateral de acuerdo con la pagina activa.

--- js/funciones.js
+++ js/funciones.js
@@ -6,3 +6,34 @@ function mostrarContrasena(){
         tipoInput.type = "password";
     }
 }
+
+/** 
+ * Modifica la clase para el item seleccionado del menu en la barra lateral 
+ * del panel admin.
+ * @return void -> Modifica la clase del item a.
+ */
+function resaltarItemActivo () {
+    //Obtener pagina activa 
+    var paginaActual = location.href;
+
+    // Obtener elementos del panel lateral
+    var panel = document.getElementById("navbar-lateral-admin");
+
+    // Obtener los hijos (los enlaces <a>)
+    const enlaces = panel.querySelectorAll('a.nav-link'); 
+
+    // Recorrer los enlaces
+    for (let i = 0; i < enlaces.length; i++) {
+        const enlace = enlaces[i];
+        if (enlace.href == paginaActual) {
+            enlace.classList.add('active');
+        } else {
+            if (enlace.classList.contains('active')) {
+                enlace.classList.remove('active');
+            }
+        }
+    }
+}
+
+// Se escucha evento para resaltar el item correspondiente en la barra lateral del panel admin
+document.addEventListener("DOMContentLoaded", resaltarItemActivo);
--- paginas/admin/navbar_lateral.php
+++ paginas/admin/navbar_lateral.php
@@ -1,4 +1,4 @@
-<nav class="nav flex-column nav-pills">
+<nav class="nav flex-column nav-pills" id="navbar-lateral-admin">
     <a class="nav-link active" href="/inicio_admin.php">Inicio</a>
     <a class="nav-link" href="/servicios_admin.php">Servicios</a>
     <a class="nav-link" href="/paginas/admin/categorias_admin.php">Categorias</a>
