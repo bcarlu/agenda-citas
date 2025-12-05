a889cdfeff6a2ec1f85e861faa4d1432f7dd41b2
Brian <briancardona87@gmail.com>
2025-12-05 16:55:43 -0500

    refactor: Se ajustan href para que carguen correctamente los archivos cuando se incluye el head en las diferentes paginas.

--- head.php
+++ head.php
@@ -6,8 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php echo $titulo; ?></title>
 	<link rel="icon" type="image/jpg" href="img/favicon.jpg">
-	<link rel="stylesheet" href="css/fontawsome/css/all.css">
-	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="/css/fontawsome/css/all.css">
+	<link rel="stylesheet" href="/css/estilos.css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,700&display=swap" rel="stylesheet">
 	<!--Bootstrap CSS-->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
@@ -15,5 +15,5 @@
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 	<!-- Funciones-->
-	<script src="js/funciones.js"></script>
+	<script src="/js/funciones.js"></script>
 </head>
